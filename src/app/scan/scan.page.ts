1f3992213680b3f1457584fc574361ee409d67d6
jayjaycrown <olajidecrown@gmail.com>
2020-07-12 19:57:15 +0100

    Fixed Student record not displaying error

--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -1,104 +1,116 @@
-import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Component, OnInit } from "@angular/core";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
 
-
-import { ListService } from '../list/list.service';
-import { Observable } from 'rxjs';
-import { Subscription } from 'rxjs';
-import { LoadingController } from '@ionic/angular';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-
+import { ListService } from "../list/list.service";
+import { Observable } from "rxjs";
+import { Subscription } from "rxjs";
+import { LoadingController } from "@ionic/angular";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-scan',
-  templateUrl: './scan.page.html',
-  styleUrls: ['./scan.page.scss'],
+  selector: "app-scan",
+  templateUrl: "./scan.page.html",
+  styleUrls: ["./scan.page.scss"],
 })
 export class ScanPage implements OnInit {
   data: any;
-  message = '';
+  message = "";
   item: any;
   nostudent;
   mainForm: FormGroup;
   listsItems: Observable<any[]>;
   private scanSub: Subscription;
-  constructor(private list: ListService,
-              private barcodeScanner: BarcodeScanner,
-              private loadingCtrl: LoadingController,
-              private formBuilder: FormBuilder, ) { }
+  constructor(
+    private list: ListService,
+    private barcodeScanner: BarcodeScanner,
+    private loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-
     this.mainForm = new FormGroup({
-      content: new FormControl(null, {
-        updateOn: 'blur',
-        validators: [Validators.required]
-      })
+      stid: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
     });
   }
 
   scan() {
     this.data = null;
     this.message = null;
-    this.barcodeScanner.scan(
-      {
+    this.barcodeScanner
+      .scan({
         showFlipCameraButton: true,
         showTorchButton: true,
-      }
-    ).then((barcodeData: any) => {
-      this.data = barcodeData.text;
-      // alert(this.data);
-      if (this.data) {
-        this.loadingCtrl.create({
-          keyboardClose: true,
-          message: `
+      })
+      .then((barcodeData: any) => {
+        this.data = barcodeData.text;
+        // alert(this.data);
+        if (this.data) {
+          this.loadingCtrl
+            .create({
+              keyboardClose: true,
+              message: `
                 <div class="custom-spinner-container">
                   <div class="custom-spinner-box"></div>
-                </div>`
-        }).then(loadingEl => {
-          loadingEl.present();
-          this.list.searchStudent(this.data).then(doc => {
-            loadingEl.dismiss();
-            // alert(doc);
-            this.item = doc;
-            console.log(doc);
-          }).catch(err => {
-            this.message = err;
-          });
-        }).catch();
-      }
-    }).catch(err => {
-      this.message = err;
-    });
+                </div>`,
+            })
+            .then((loadingEl) => {
+              loadingEl.present();
+              this.list
+                .searchStudent(this.data)
+                .then((doc) => {
+                  loadingEl.dismiss();
+                  // alert(doc);
+                  this.item = doc;
+                  console.log(doc);
+                })
+                .catch((err) => {
+                  this.message = err;
+                });
+            })
+            .catch();
+        }
+      })
+      .catch((err) => {
+        this.message = err;
+      });
   }
 
-  searchStudent() {
-    this.loadingCtrl.create({
-      message: `
+  searchStudent(event) {
+    this.loadingCtrl
+      .create({
+        message: `
                 <div class="custom-spinner-container">
                   <div class="custom-spinner-box"></div>
-                </div>`
-    }).then(loadingEl => {
-      loadingEl.present();
-      this.list.searchStudent(this.mainForm.value.content).then(doc => {
-        this.nostudent = false;
-        loadingEl.dismiss();
-        this.resetForm();
-        // alert(doc);
-        this.item = doc;
-        console.log(doc);
-      }).catch(err => {
-        loadingEl.dismiss();
-        alert(err);
+                </div>`,
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.list
+          .searchStudent(this.mainForm.value.stid)
+          .then((doc) => {
+            this.nostudent = false;
+            loadingEl.dismiss();
+            this.resetForm();
+            // alert(doc);
+            this.item = doc;
+            // console.log(doc);
+          })
+          .catch((err) => {
+            loadingEl.dismiss();
+            alert(err);
+          });
       });
-    });
   }
 
   resetForm() {
     this.mainForm.reset();
   }
-
-
-
-
 }
